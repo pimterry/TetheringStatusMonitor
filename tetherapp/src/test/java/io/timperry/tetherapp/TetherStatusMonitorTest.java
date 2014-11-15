2dc4678498e36a1592442a38faae0c166495adda
pimterry <pimterry@gmail.com>
2014-11-15 16:17:29 +0000

    Working robolectric tests

---
+++ tetherapp/src/test/java/io/timperry/tetherapp/TetherStatusMonitorTest.java
@@ -0,0 +1,14 @@
+package io.timperry.tetherapp;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class TetherStatusMonitorTest {
+
+    @Test
+    public void testSomething() {
+        assertEquals(0, 0);
+    }
+
+}
